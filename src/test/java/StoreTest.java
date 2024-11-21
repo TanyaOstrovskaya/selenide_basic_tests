1d22856ff4405102d8a7536cf7810e8131bb4050
Vivien <bbrother92@gmail.com>
2024-11-21 17:29:57 +0300

    Add new tests

---
+++ src/test/java/StoreTest.java
@@ -0,0 +1,64 @@
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+import pages.StoreLoginPage;
+
+import java.util.stream.Stream;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@Slf4j
+public class StoreTest extends BaseTest {
+
+
+    public static final String STANDARD_USER = "standard_user";
+    public static final String SECRET = "secret_sauce";
+    public static final String BLOCKED_USER = "locked_out_user";
+
+
+
+    @Test
+    public void userCanLogin() {
+        StoreLoginPage page = new StoreLoginPage();
+        page.navigate();
+        page.login(STANDARD_USER, SECRET);
+        page.checkUserLogged();
+    }
+
+    @Test
+    public void cantLoginAsBlockedUser() {
+        StoreLoginPage page = new StoreLoginPage();
+        page.navigate();
+        page.login(BLOCKED_USER, SECRET);
+        assertThat(page.getLoginError()).isEqualTo("Epic sadface: Sorry, this user has been locked out.");
+
+    }
+
+
+    static Stream<Arguments> invalidLoginDataProvider() {
+        return Stream.of(
+                Arguments.of("", SECRET, "Epic sadface: Username is required"),
+                Arguments.of(STANDARD_USER, "", "Epic sadface: Password is required"),
+                Arguments.of("invalidUser", SECRET, "Epic sadface: Username and password do not match any user in this service"),
+                Arguments.of(STANDARD_USER, "wrong", "Epic sadface: Username and password do not match any user in this service"),
+                Arguments.of("", "", "Epic sadface: Username is required")
+        );
+    }
+    @ParameterizedTest(name = "Test {index}: username={0}, password={1}, errormsg={2} ")
+    @MethodSource("invalidLoginDataProvider")
+    public void shouldNotLoginWithInvalidFields(String username, String password, String errormsg) {
+        StoreLoginPage page = new StoreLoginPage();
+        page.navigate();
+        page.login(username, password);
+        assertThat(page.getLoginError()).isEqualTo(errormsg);
+    }
+
+
+
+
+}
+
+
+
